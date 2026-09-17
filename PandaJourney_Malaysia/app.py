f70c103374f64922d44859cbc059374f13082a58
limkx-wm24-eng <limkx-wm24@gmail.com>
2026-09-17 08:04:50 +0800

    PUSH

--- PandaJourney_Malaysia/app.py
+++ PandaJourney_Malaysia/app.py
@@ -354,12 +354,12 @@ def _verify_firebase_id_token(id_token: str) -> dict:
 @app.errorhandler(404)
 def page_not_found(error):
     flash("Page not found. Redirected to Attractions.", "warning")
-    return redirect(url_for("attraction"))
+    return redirect(url_for("attractions"))
 
 @app.errorhandler(500)
 def internal_server_error(error):
     flash("Something went wrong. Please try again.", "danger")
-    return redirect(url_for("attraction"))
+    return redirect(url_for("attractions"))
 
 @app.route("/session-login", methods=["POST"])
 def session_login():
